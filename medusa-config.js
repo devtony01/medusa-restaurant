60af048e606ec6058f23774a6532cb07c17dd505
devtony01 <chibuezeogbuji01@gmail.com>
2024-06-18 17:08:37 +0100

    remove admin, medusa-restaurant modules

--- medusa-config.js
+++ medusa-config.js
@@ -42,23 +42,6 @@ const plugins = [
       upload_dir: "uploads",
     },
   },
-  {
-    resolve: "@medusajs/admin",
-    /** @type {import('@medusajs/admin').PluginOptions} */
-    options: {
-      autoRebuild: true,
-      develop: {
-        open: process.env.OPEN_BROWSER !== "false",
-      },
-    },
-  },
-  {
-    resolve: `medusa-restaurant-ui`,
-    options: {
-      enableUI: true,
-    },
-  },
-
 ];
 
 const modules = {
